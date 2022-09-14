baeab6d2b85ad2d397a74121598dc8549a74aade
Marcin <marcinmichalak.m@gmail.com>
2022-09-14 09:30:22 +0200

    fix clicker

--- js/plausible-tracking.js
+++ js/plausible-tracking.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var clickers = document.querySelectorAll("a[data-track]");
   clickers.forEach(function (clicker) {
-    clickerName = clicker.dataset("data-track");
+    clickerName = clicker.dataset.track;
     clicker.addEventListener("click", function (event) {
       plausible("Click", { props: { CTA: clickerName } });
     });
