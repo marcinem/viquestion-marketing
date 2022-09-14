dc3cd330f9e2c8dcaa7eb57edb9a8b1f56c45633
Marcin <marcinmichalak.m@gmail.com>
2022-09-14 09:41:07 +0200

    fix

--- js/plausible-tracking.js
+++ js/plausible-tracking.js
@@ -2,9 +2,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   var clickers = document.querySelectorAll("a[data-track]");
   clickers.forEach(function (clicker) {
-    clickerName = clicker.dataset.track;
     clicker.addEventListener("click", function (event) {
-      plausible("Click", { props: { CTA: clickerName } });
+      plausible("Click", { props: { CTA: clicker.dataset.track } });
     });
   });
 });
