f096798ae98824782e2da22d88e6548ee418564d
Marcin <marcinmichalak.m@gmail.com>
2022-09-19 20:45:08 +0200

    add demo tracking

--- js/plausible-tracking.js
+++ js/plausible-tracking.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
   clickers.forEach(function (clicker) {
     clicker.addEventListener("click", function (event) {
       plausible("Click", { props: { CTA: clicker.dataset.track } });
-    });
+    }, {once: true});
   });
+});
+
+
+function isCalendlyEvent(e) {
+  return e.origin === "https://calendly.com" && e.data.event && e.data.event.indexOf("calendly.") === 0;
+};
+ 
+window.addEventListener("message", function(e) {
+  if(isCalendlyEvent(e) && e.data.event === "calendly.event_scheduled") {
+    plausible("Demo Booked", { props: { CTA: clicker.dataset.track } });
+  }
 });
